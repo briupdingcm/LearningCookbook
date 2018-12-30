07edd810c3fd58636b00addfce87ed9b960ed618
briupdingcm <dingcm@briup.com>
2018-12-30 15:40:23 +0800

    new file be added for logistic regression

--- Code/Chap06/multi_layer.py
+++ Code/Chap06/multi_layer.py
@@ -1,28 +1,62 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-
-
-
-x = [
-    [0., 0.],
-    [1., 0.],
-    [0., 1.],
-    [1., 1.],
-]
-y = [[1], [0], [0], [1]]
+import datetime as dt
 
 input_layer_nodes = 2
 
-output_layer_nodes = 1
+output_layer_nodes = 2
 
-first_hidden_nodes = 6
+first_hidden_nodes = 10
 
-second_hidden_nodes = 3
+second_hidden_nodes = 10
+
+samples_num = 30000
+
+iteration = 100000
+
+batch_size = 1000
+
+learning_rate = 0.2
+
+def error_rate(Y, Z):
+    t1 = [1.0 if e1 >= e2 else 0.0 for [e1, e2] in Y]
+    t2 = [1.0 if e1 >= e2 else 0.0 for [e1, e2] in Z]
+    t = [1.0 if e1 == e2 else 0.0 for (e1, e2) in zip(t1, t2)]
+    succ = np.sum(t)
+    return succ / len(Y)
 
 
+def dataset():
+    x_data = np.reshape(np.random.rand(samples_num*input_layer_nodes), [samples_num, input_layer_nodes])
+    y_data = [[1., 0.] if (x0 >= 0.5 and x1 >= 0.5) or (x0 <= 0.5 and x1 <= 0.5) else [0., 1.] for [x0, x1] in x_data]
+    return np.array(x_data), np.array(y_data)
 
 
+x, y = dataset()
+
+
+def get_data():
+    rand_index = np.random.choice(samples_num, size=batch_size)
+    rand_x = x[rand_index]
+    rand_y = y[rand_index]
+    return rand_x, rand_y
+
+
+def draw():
+    c1 = np.array([x0 for (x0, [y0, y1]) in zip(x, y) if y0 > y1])
+    c2 = np.array([x0 for (x0, [y0, y1]) in zip(x, y) if y0 <= y1])
+
+    plt.title('Sample')
+
+    plt.plot(c1[:, 0], c1[:, 1], 'b.')
+    plt.plot(c2[:, 0], c2[:, 1], 'r.')
+    plt.show()
+
+
+draw()
+
+start_time = dt.datetime.now()
 
 g = tf.Graph()
 
@@ -50,18 +84,41 @@ with g.as_default():
         final_out = tf.sigmoid(tf.add(tf.matmul(second_out, w3), b3))
 
     with tf.name_scope('loss'):
-        loss = tf.reduce_mean(tf.square(first_out - y_target))
+        loss = tf.reduce_mean(tf.square(final_out - y_target))
 
     with tf.name_scope('train'):
-        my_opt = tf.train.GradientDescentOptimizer(0.01)
+        my_opt = tf.train.GradientDescentOptimizer(learning_rate)
         train_step = my_opt.minimize(loss)
 
+    loss_vec = []
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
+        for i in range(iteration):
+            rand_x, rand_y = get_data()
 
-        for i in range(10000):
-            sess.run(train_step, feed_dict={x_input: x, y_target: y})
-            print('i = ' + str(i) + ' ' )
-            print(sess.run(loss, feed_dict={x_input: x, y_target: y}))
-            print(sess.run(final_out, feed_dict={x_input: x, y_target: y}))
+            sess.run(train_step, feed_dict={x_input: rand_x, y_target: rand_y})
+            error = sess.run(loss, feed_dict={x_input: rand_x, y_target: rand_y})
+            loss_vec.append(error)
+            if i % 100 == 0:
+                print("loss: " + str(error))
+        y_predict = sess.run(final_out, feed_dict={x_input: x, y_target: y})
+        end_time = dt.datetime.now()
+        print(end_time - start_time)
+
+    with tf.name_scope('draw'):
+        c1 = np.array([x0 for (x0, [y0, y1]) in zip(x, y_predict) if y0 <= y1])
+        c2 = np.array([x0 for (x0, [y0, y1]) in zip(x, y_predict) if y0 > y1])
+
+        plt.title('XOR Result: ' + str(error_rate(y, y_predict)))
+
+        plt.plot(c1[:, 0], c1[:, 1], 'b.')
+        plt.plot(c2[:, 0], c2[:, 1], 'r.')
+        plt.show()
+
+        plt.plot(loss_vec, 'r.')
+        plt.title('Loss (MSE) per Generation')
+        plt.xlabel('Generation')
+        plt.ylabel('Loss')
+        plt.legend(loc='upper right')
+        plt.show()
