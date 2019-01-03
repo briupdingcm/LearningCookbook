7493f2fee80c115722e9b58c5b26da62cdc8390c
briupdingcm <dingcm@briup.com>
2019-01-03 17:19:16 +0800

    new file be added for logistic regression

--- Code/Chap05/kNN.py
+++ Code/Chap05/kNN.py
@@ -25,10 +25,12 @@ x_vals, y_vals = load_dataset()
 def split(indices):
     return x_vals[indices], y_vals[indices]
 
+
 def choice():
     train_indices = np.random.choice(len(x_vals), round(len(x_vals)*0.8), replace=False)
     test_indices = np.array(list(set(range(len(x_vals))) - set(train_indices)))
     return train_indices, test_indices
+
 
 train_indices, test_indices = choice()
 x_vals_train, y_vals_train = split(train_indices)
@@ -49,4 +51,21 @@ with tf.Session() as sess:
     with tf.name_scope('inference'):
         top_k_xvals, top_k_indices = tf.nn.top_k(tf.negative(distance), k=k)
 
+    x_sums = tf.expand_dims(tf.reduce_mean(top_k_xvals, 1), 1)
+    x_sums_repeated = tf.matmul(x_sums, tf.ones([1, k], tf.float32))
+    x_vals_weights = tf.expand_dims(tf.div(top_k_xvals, x_sums_repeated), 1)
 
+    top_k_yvals = tf.gather(y_target_train, top_k_indices)
+    prediction = tf.squeeze(tf.matmul(x_vals_weights, top_k_yvals), squeeze_dims=[1])
+    mse = tf.div(tf.reduce_mean(tf.square(tf.subtract(prediction, y_target_test))), batch_size)
+
+    num_loops = int(np.ceil(len(x_vals_test) / batch_size))
+
+    for i in range(num_loops):
+        min_index = i * batch_size
+        max_index = min((i + 1) * batch_size, len(x_vals_train))
+        x_batch = x_vals_test[min_index:max_index]
+        y_batch = y_vals_test[min_index:max_index]
+        prediction = sess.run(prediction, feed_dict={x_data_train: x_vals_train, x_data_test: x_batch, y_target_train: y_vals_train, y_target_test: y_batch})
+        batch_mse = sess.run(mse, feed_dict={x_data_train: x_vals_train, x_data_test: x_batch, y_target_train: y_vals_train, y_target_test: y_batch})
+        print('Batch # ' + str(i + 1) + ' MSE: ' + str(batch_mse))
